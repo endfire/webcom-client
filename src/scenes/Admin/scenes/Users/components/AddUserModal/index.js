31c3db9a54a57d74f290c07ec084fd74e40d2f56
calvinbrewer <brewercalvinj@gmail.com>
2016-07-25 21:36:07 -0600

    Add admin user page

---
+++ src/scenes/Admin/scenes/Users/components/AddUserModal/index.js
@@ -0,0 +1,126 @@
+import React, { PropTypes, Component } from 'react';
+import { Button, Card, Container, Row, Col, withModal } from 'paintcan';
+import { connect } from 'react-redux';
+import { CREATE_REQUEST } from '../../../../../../actionTypes';
+
+const AddUserModal = withModal(
+  ({ isOpen, openModal }) => (
+    <Button active={isOpen} onClick={openModal} color="primary">
+      Add a new user
+    </Button>
+  ),
+  ({ closeModal, createUser, isCreateLoading }) => (
+    <AddUserDialog
+      closeModal={closeModal}
+      createUser={createUser}
+      isCreateLoading={isCreateLoading}
+    />
+  ),
+);
+
+class AddUserDialog extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      role: '',
+    };
+
+    this.submitHandler = this.submitHandler.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  submitHandler(e) {
+    e.preventDefault();
+    const { isCreateLoading, createUser, closeModal } = this.props;
+
+    if (isCreateLoading) return;
+
+    const { name, email, role } = this.state;
+    createUser(name, email, role);
+    closeModal();
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { submitHandler, handleChange } = this;
+    const { closeModal } = this.props;
+
+    return (
+      <Container fluid>
+        <Row align={{ xs: 'center' }}>
+          <Col size={{ xs: 10, lg: 4 }} align={{ xs: 'start' }}>
+            <Card>
+              <h3>Add new user</h3>
+              <form onSubmit={submitHandler}>
+                <label htmlFor="name">Name</label><br />
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  onChange={handleChange}
+                  value={this.state.name}
+                /><br />
+                <label htmlFor="email">Email</label><br />
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  onChange={handleChange}
+                  value={this.state.email}
+                /><br />
+                <label htmlFor="role">Role</label><br />
+                <input
+                  type="text"
+                  id="role"
+                  name="role"
+                  onChange={handleChange}
+                  value={this.state.role}
+                /><br />
+                <input type="submit" value="Save Change" />
+              </form>
+              <Button onClick={closeModal}>
+                Cancel
+              </Button>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isCreateLoading: state.store.isLoading.CREATE,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  createUser: (name, email, role) => dispatch({
+    type: CREATE_REQUEST,
+    payload: {
+      type: 'user',
+      record: {
+        name,
+        email,
+        role,
+      },
+    },
+  }),
+});
+
+AddUserDialog.propTypes = {
+  closeModal: PropTypes.func,
+  createUser: PropTypes.func,
+  isCreateLoading: PropTypes.bool,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(AddUserModal);
--- src/scenes/Admin/scenes/Users/index.js
+++ src/scenes/Admin/scenes/Users/index.js
@@ -1,7 +1,83 @@
-import React from 'react';
+import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
+import { Container, Row, Col } from 'paintcan';
+import List from './components/List';
+import AddUserModal from './components/AddUserModal';
+import { FIND_REQUEST, DELETE_REQUEST } from '../../../../actionTypes';
 
-const Users = () => (
-  <p>Users here</p>
-);
+class Users extends Component {
+  constructor(props) {
+    super(props);
 
-export default Users;
+    this.find = this.find.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  componentDidMount() {
+    this.find();
+  }
+
+  find() {
+    const { findUsers } = this.props;
+    findUsers();
+  }
+
+  handleDelete(id) {
+      // const { isDeleteLoading, deleteUser } = this.props;
+    const { isDeleteLoading } = this.props;
+
+    if (isDeleteLoading) return;
+
+    console.log(`Delete user ${id}`);
+    // FIXME: need to fix saga...
+    // deleteUser(id); // dispatch action
+  }
+
+  render() {
+    const { users } = this.props;
+
+    return (
+      <Container fluid><br />
+        <Row>
+          <Col size={{ xs: 4 }} align={{ xs: 'left' }}>
+            <AddUserModal /><br /><br /><br />
+            {users ? <List items={users} handleDelete={this.handleDelete} /> : 'Loading...'}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  users: state.store.entities.users,
+  isDeleteLoading: state.store.isLoading.DELETE,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  findUsers: () => dispatch({
+    type: FIND_REQUEST,
+    payload: {
+      type: 'user',
+    },
+  }),
+  deleteUser: (id) => dispatch({
+    type: DELETE_REQUEST,
+    payload: {
+      type: 'user',
+      id,
+    },
+  }),
+});
+
+Users.propTypes = {
+  users: PropTypes.object,
+  findUsers: PropTypes.func,
+  isDeleteLoading: PropTypes.bool,
+  deleteUser: PropTypes.func,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Users);
