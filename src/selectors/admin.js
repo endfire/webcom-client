871a5bfbcb670707ad8a5361e25b6a5484d44b86
Eric Bukowski <john.eric.bukowski@gmail.com>
2016-08-29 17:35:05 -0600

    Update selectors

--- src/selectors/admin.js
+++ src/selectors/admin.js
@@ -1,7 +1,25 @@
 import { createSelector } from 'reselect';
 
-const getForms = (state) => state.store.getIn(['entities', 'forms']);
+const getStore = (state) => state.store;
 const getUserRole = (state) => state.session.get('role');
+const getForms = createSelector(
+  [getStore],
+  (store) => store.getIn(['entities', 'forms'])
+);
+
+export const getCurrentBrand = (brandID) => (
+  createSelector(
+    [getStore],
+    (store) => store.getIn(['entities', 'brands', brandID])
+  )
+);
+
+export const getCurrentBrandForms = (brandID) => (
+  createSelector(
+    [getForms],
+    (forms) => forms.filter(val => val.get('brand') === brandID)
+  )
+);
 
 export const getCanUserDelete = createSelector(
   [getUserRole],
@@ -16,11 +34,4 @@ export const getCanUserAddUsers = createSelector(
 export const getCanUserAccessOBG = createSelector(
   [getUserRole],
   (userRole) => userRole === '1' || userRole === '2'
-);
-
-export const getCurrentBrandForms = (brandID) => (
-  createSelector(
-    [getForms],
-    (forms) => forms.filter(val => val.get('brand') === brandID)
-  )
 );
--- src/selectors/company.js
+++ src/selectors/company.js
@@ -1,10 +1,19 @@
 import { createSelector } from 'reselect';
 
-const getPeople = (state) => state.store.getIn(['entities', 'people']);
+const getStore = (state) => state.store;
+const getPeople = createSelector(
+  [getStore],
+  (store) => store.getIn(['entities', 'people'])
+);
 
 export const getSessionID = (state) => state.session.get('id');
 
 export const getNonDeletedPeople = createSelector(
   [getPeople],
   (people) => people.filter(val => val.get('meta') && !val.getIn(['meta', 'archived']))
+);
+
+export const getLoggedInCompany = createSelector(
+  [getSessionID, getStore],
+  (sessionID, store) => store.getIn(['entities', 'companies', sessionID])
 );
