e5213c3dc1ea9dd2ca63f551aa358aa65a5331ac
Eric Bukowski <john.eric.bukowski@gmail.com>
2016-08-29 17:38:37 -0600

    Add `company` to state tree after login

--- src/scenes/Company/index.js
+++ src/scenes/Company/index.js
@@ -1,42 +1,63 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import { Container, Row, Col, Button } from 'paintcan';
 import { Link } from 'react-router';
+import { getSessionID } from 'selectors/company';
 import * as types from 'constants/actionTypes';
+import * as actions from 'actions/store';
 
-const Company = ({ children, logoutRequest }) => {
-  const handleLogout = () => {
-    logoutRequest();
-  };
+class Company extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div>
-      <Container fluid style={{ backgroundColor: 'yellow' }}>
-        <Row>
-          <Col size={{ xs: 2 }} align={{ xs: 'center' }}>
-            <Link to="/company">Webcom</Link>
-          </Col>
-          <Col size={{ xs: 1 }} align={{ xs: 'center' }}>
-            <Link to="/company/listings">Listings</Link>
-          </Col>
-          <Col size={{ xs: 1 }} align={{ xs: 'center' }}>
-            <Link to="/company/people">People</Link>
-          </Col>
-          <Col size={{ xs: 1 }} align={{ xs: 'center' }}>
-            <Link to="/company/settings">Settings</Link>
-          </Col>
-          <Col size={{ xs: 6 }} align={{ xs: 'center' }}>
-            <Button onClick={handleLogout}>Logout</Button>
-          </Col>
-        </Row>
-      </Container>
+    this.handleLogout = this.handleLogout.bind(this);
+  }
 
-      {children}
-    </div>
-  );
-};
+  componentDidMount() {
+    const { fetchCompany, companyID } = this.props;
+
+    fetchCompany(companyID);
+  }
+
+  handleLogout() {
+    this.props.logoutRequest();
+  }
+
+  render() {
+    return (
+      <div>
+        <Container fluid style={{ backgroundColor: 'yellow' }}>
+          <Row>
+            <Col size={{ xs: 2 }} align={{ xs: 'center' }}>
+              <Link to="/company">Webcom</Link>
+            </Col>
+            <Col size={{ xs: 1 }} align={{ xs: 'center' }}>
+              <Link to="/company/listings">Listings</Link>
+            </Col>
+            <Col size={{ xs: 1 }} align={{ xs: 'center' }}>
+              <Link to="/company/people">People</Link>
+            </Col>
+            <Col size={{ xs: 1 }} align={{ xs: 'center' }}>
+              <Link to="/company/settings">Settings</Link>
+            </Col>
+            <Col size={{ xs: 6 }} align={{ xs: 'center' }}>
+              <Button onClick={this.handleLogout}>Logout</Button>
+            </Col>
+          </Row>
+        </Container>
+
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  companyID: getSessionID(state),
+});
 
 const mapDispatchToProps = (dispatch) => ({
+  fetchCompany: (companyID) => dispatch(actions.fetchRecord('company', companyID)),
   logoutRequest: () => dispatch({
     type: types.LOGOUT_REQUEST,
     payload: '/company-login',
@@ -44,11 +65,13 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 Company.propTypes = {
+  companyID: PropTypes.string,
   children: PropTypes.any,
+  fetchCompany: PropTypes.func,
   logoutRequest: PropTypes.func,
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps,
 )(Company);
