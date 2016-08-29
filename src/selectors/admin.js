4c4f4325afdfeeeb2fd2217a697b04e5b39a0dea
Eric Bukowski <john.eric.bukowski@gmail.com>
2016-08-29 08:05:31 -0600

    Update `admin` and `company` selectors

--- src/selectors/admin.js
+++ src/selectors/admin.js
@@ -1,11 +1,16 @@
 import { createSelector } from 'reselect';
 
-const getSession = (state) => state.session;
 const getForms = (state) => state.store.getIn(['entities', 'forms']);
+const getUserRole = (state) => state.session.get('role');
 
-export const getUserRole = createSelector(
-  [getSession],
-  (session) => session.get('role')
+export const getCanUserDelete = createSelector(
+  [getUserRole],
+  (userRole) => userRole === '1'
+);
+
+export const getCanUserAddUsers = createSelector(
+  [getUserRole],
+  (userRole) => userRole === '1' || userRole === '2'
 );
 
 export const getCurrentBrandForms = (brandID) => (
--- src/selectors/company.js
+++ src/selectors/company.js
@@ -2,6 +2,8 @@ import { createSelector } from 'reselect';
 
 const getPeople = (state) => state.store.getIn(['entities', 'people']);
 
+export const getSessionID = (state) => state.session.get('id');
+
 export const getNonDeletedPeople = createSelector(
   [getPeople],
   (people) => people.filter(val => val.get('meta') && !val.getIn(['meta', 'archived']))
