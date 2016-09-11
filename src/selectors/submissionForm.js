26292f95f85035cc26d1f30149100a6999f5eb0f
Eric Bukowski <john.eric.bukowski@gmail.com>
2016-09-11 10:04:00 -0600

    Create `submissionForm` selector

---
+++ src/selectors/submissionForm.js
@@ -0,0 +1,48 @@
+import { createSelector } from 'reselect';
+
+const getStore = (state) => state.store;
+
+const getFields = createSelector(
+  [getStore],
+  (store) => store.getIn(['entities', 'fields'])
+);
+
+const getPayments = createSelector(
+  [getStore],
+  (store) => store.getIn(['entities', 'payments'])
+);
+
+const getItems = createSelector(
+  [getStore],
+  (store) => store.getIn(['entities', 'items'])
+);
+
+const getForms = createSelector(
+  [getStore],
+  (store) => store.getIn(['entities', 'forms'])
+);
+
+export const getCurrentFormFields = (formID) => (
+  createSelector(
+    [getFields],
+    (fields) => fields.filter(val => val.get('form') === formID)
+  )
+);
+
+export const getCurrentFormPayment = (formID) => (
+  createSelector(
+    [getPayments],
+    (payments) => payments.filter(val => val.get('form') === formID)
+  )
+);
+
+export const getCurrentFormPaymentItems = (formID) => (
+  createSelector(
+    [getItems, getForms],
+    (items, forms) => {
+      const currentFormPaymentID = forms.getIn([formID, 'payment']);
+
+      return items.filter(val => val.get('payment') === currentFormPaymentID);
+    }
+  )
+);
