8bc8b8c8abaa249d5d622325874f6e98fffe941e
Eric Bukowski <john.eric.bukowski@gmail.com>
2016-09-23 09:17:07 -0600

    Add links to published form endpoint

--- src/scenes/Admin/scenes/Brand/scenes/BrandForms/components/List/components/ListItem/index.js
+++ src/scenes/Admin/scenes/Brand/scenes/BrandForms/components/List/components/ListItem/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-no-bind */
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
-import { ButtonGroup } from 'paintcan';
+import { Button, ButtonGroup } from 'paintcan';
 import { DeleteModal } from 'scenes/components';
 import { EditFormRecipientsModal } from '../';
 import styles from './styles.scss';
@@ -12,6 +12,9 @@ const ListItem = ({ item, brandID, handleDelete, canUserDelete, updateForm, isUp
       <Link to={`/admin/brands/${brandID}/forms/${item.get('id')}`}>{item.get('name')}</Link>
     </div>
     <ButtonGroup spaced>
+      {item.get('didPublish')
+        && <Link to={`/form/${item.get('id')}`}><Button size="sm">To published form</Button></Link>
+      } &nbsp;
       <EditFormRecipientsModal
         form={item}
         updateForm={updateForm}
