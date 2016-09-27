5244ed95cca685483393dac902d631903ed31be7
calvinbrewer <brewercalvinj@gmail.com>
2016-09-27 12:07:01 -0600

    Change host to webcom-server

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,8 +1,7 @@
 import Sdk from './sdk';
 
 const api = new Sdk({
-  // host: 'https://webcom-server.herokuapp.com',
-  host: 'http://localhost:4200',
+  host: 'https://webcom-server.herokuapp.com',
 });
 
 export { api };
