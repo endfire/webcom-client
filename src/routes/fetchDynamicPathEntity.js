53d67f143759e0f6fa8c83e0e0dc492b4b8a23b5
calvinbrewer <brewercalvinj@gmail.com>
2016-10-03 13:06:51 -0600

    Fix `companyOne` selector and add fields to company settings

--- src/routes/fetchDynamicPathEntity.js
+++ src/routes/fetchDynamicPathEntity.js
@@ -2,7 +2,10 @@ import * as actions from 'actions/store';
 
 export default (store, api, entity, dynamicPath) => (nextState, replace, callback) => {
   const id = dynamicPath ? nextState.params[dynamicPath] : localStorage.id;
-  const done = () => callback();
+  const done = (m) => {
+    console.log('done', m);
+    return callback();
+  };
 
   return api.fetch(entity, id)
     .then((record) => store.dispatch(actions.syncStore(entity, record)))
