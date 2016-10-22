451fe49aa8c138ec95f790daeea40adf578d0b11
Eric Bukowski <john.eric.bukowski@gmail.com>
2016-10-22 14:51:56 -0600

    Add confirm delete functionality

--- src/scenes/components/DeleteModal/index.js
+++ src/scenes/components/DeleteModal/index.js
@@ -22,7 +22,15 @@ class DeleteDialog extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      confirmDelete: '',
+    };
+
     this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.renderCancelButton = this.renderCancelButton.bind(this);
+    this.renderDeleteButton = this.renderDeleteButton.bind(this);
+    this.renderConfirmDelete = this.renderConfirmDelete.bind(this);
   }
 
   handleClick(e) {
@@ -32,7 +40,57 @@ class DeleteDialog extends Component {
     this.props.closeModal();
   }
 
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  renderCancelButton() {
+    return (
+      <div className={styles.buttons}>
+        <button
+          className={cx(styles.button, styles.cancel)}
+          type="button"
+          onClick={this.props.closeModal}
+        >
+          Cancel
+        </button>
+      </div>
+    );
+  }
+
+  renderDeleteButton() {
+    return (
+      <div className={styles.buttons}>
+        <button
+          className={cx(styles.button, styles.confirm)}
+          type="button"
+          onClick={this.handleClick}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  renderConfirmDelete() {
+    return (
+      <fieldset>
+        <input
+          type="text"
+          id="confirmDelete"
+          name="confirmDelete"
+          onChange={this.handleChange}
+          value={this.state.confirmDelete}
+          placeholder="I WANT TO DELETE"
+        />
+      </fieldset>
+    );
+  }
+
   render() {
+    const { renderConfirmDelete, renderDeleteButton, renderCancelButton } = this;
+
     return (
       <div className={styles.wrapper}>
         <div className={styles.trash}>
@@ -41,22 +99,11 @@ class DeleteDialog extends Component {
             Are you sure you want to delete this?
           </span>
         </div>
-        <div className={styles.buttons}>
-          <button
-            className={cx(styles.button, styles.confirm)}
-            type="button"
-            onClick={this.handleClick}
-          >
-            Yes
-          </button>
-          <button
-            className={cx(styles.button, styles.cancel)}
-            type="button"
-            onClick={this.props.closeModal}
-          >
-            No
-          </button>
-        </div>
+        {renderConfirmDelete()}
+        {this.state.confirmDelete === 'I WANT TO DELETE'
+          ? renderDeleteButton()
+          : renderCancelButton()
+        }
       </div>
     );
   }
